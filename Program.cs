159e398f0f4e45dae67db21ffc0e9eab183bf261
Nuno Ribeiro <nmpribeiro@gmail.com>
2021-02-08 12:26:45 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Antlr4.Runtime;
+
+namespace prog_lang
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			string typescript = "let employeeName = \"John\";\n\tconsole.log(employeeName);";
+			//ICharStream target = new AntlrInputStream(typescript);
+
+			//ITokenSource lexer = new TypeScriptLexer(target);
+			//ITokenStream tokens = new CommonTokenStream(lexer);
+			//TypeScriptParser parser = new TypeScriptParser(tokens);
+
+			//TypeScriptParser.LiteralContext result = parser.literal();
+			//Console.Write(result.ToString());
+
+
+			try
+			{
+				var stream = new AntlrInputStream(typescript);
+				var lexer = new TypeScriptLexer(stream);
+				var tokenStream = new CommonTokenStream(lexer);
+				var parser = new TypeScriptParser(tokenStream);
+				//parser.program();
+				TypeScriptParser.ProgramContext programReturn = parser.program();
+
+				var tree = programReturn.ToStringTree();
+				Console.Write(tree);
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+
+			Console.Write("Press any key to continue...");
+			Console.ReadKey(true);
+		}
+	}
+}
