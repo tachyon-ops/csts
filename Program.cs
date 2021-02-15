a20ea71f79c2cf6e2fbb03167d791e297589b795
Nuno Ribeiro <nmpribeiro@gmail.com>
2021-02-15 09:42:00 +0100

    Preparing to think about how to implement AST for intermediate code representation

--- Program.cs
+++ Program.cs
@@ -1,36 +1,121 @@
 ﻿using System;
 using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+using TypeScriptNative.src;
 
 namespace prog_lang
 {
+
+	class TreeListner : IParseTreeListener
+	{
+		int forLevel = 0;
+
+		public void EnterEveryRule(ParserRuleContext ctx)
+		{
+			this.forLevel++;
+			printInfo("EnterEveryRule", ctx);
+		}
+
+		public void ExitEveryRule(ParserRuleContext ctx)
+		{
+			this.forLevel--;
+			printInfo("ExitEveryRule", ctx);
+		}
+
+		public void VisitErrorNode(IErrorNode node)
+		{
+
+		}
+
+		public void VisitTerminal(ITerminalNode node)
+		{
+
+		}
+
+		private void printInfo(String forType, ParserRuleContext ctx)
+		{
+			if (this.forLevel == 0)
+			{
+				Console.WriteLine("\nExplore CST:");
+				Explore(ctx);
+				Console.WriteLine("\nExplore pre-AST (we can use this):");
+				ExploreAST(ctx);
+			}
+		}
+
+		// http://franckgaspoz.fr/en/first-steps-with-antlr4-in-csharp/
+		void Explore(ParserRuleContext ctx, int indentLevel = 0)
+		{
+			var ruleName = TypeScriptParser.ruleNames[ctx.RuleIndex];
+			var sep = "".PadLeft(indentLevel);
+			Console.WriteLine(sep + ruleName);
+			sep = "".PadLeft(indentLevel + 4);
+			foreach (var c in ctx.children)
+			{
+				if (c is ParserRuleContext)
+					Explore((ParserRuleContext)c, indentLevel + 4);
+				else
+					Console.WriteLine(sep + c.ToString());
+			}
+		}
+
+		void ExploreAST(ParserRuleContext ctx, int indentLevel = 0)
+		{
+			var ruleName = TypeScriptParser.ruleNames[ctx.RuleIndex];
+			var sep = "".PadLeft(indentLevel);
+			bool keepRule = ctx.ChildCount > 1;
+			if (keepRule)
+				Console.WriteLine(sep + ruleName);
+			foreach (var c in ctx.children)
+			{
+				if (c is ParserRuleContext)
+					ExploreAST((ParserRuleContext)c, indentLevel + ((keepRule) ? 4 : 0));
+				else
+				{
+					var sep2 =
+						"".PadLeft(indentLevel + ((keepRule) ? 4 : 0));
+					Console.WriteLine(sep2 + c.ToString());
+				}
+			}
+		}
+	}
+
 	class Program
 	{
-		static void Main(string[] args)
+
+
+		static int Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("TS NATIVE!");
 
-			string typescript = "let employeeName = \"John\";\n\tconsole.log(employeeName);";
-			//ICharStream target = new AntlrInputStream(typescript);
-
-			//ITokenSource lexer = new TypeScriptLexer(target);
-			//ITokenStream tokens = new CommonTokenStream(lexer);
-			//TypeScriptParser parser = new TypeScriptParser(tokens);
-
-			//TypeScriptParser.LiteralContext result = parser.literal();
-			//Console.Write(result.ToString());
-
+			string program = @"
+let a = 1;
+let b = 2;
+print(a + b);
+";
 
 			try
 			{
-				var stream = new AntlrInputStream(typescript);
+				var stream = new AntlrInputStream(program);
 				var lexer = new TypeScriptLexer(stream);
 				var tokenStream = new CommonTokenStream(lexer);
 				var parser = new TypeScriptParser(tokenStream);
-				//parser.program();
-				TypeScriptParser.ProgramContext programReturn = parser.program();
 
-				var tree = programReturn.ToStringTree();
-				Console.Write(tree);
+				IParseTree tree = parser.program();
+				Console.WriteLine("parse tree (LISP style): \n" + tree.ToStringTree(parser));
+
+				parser.program();
+
+
+				parser.RemoveErrorListeners();
+				parser.AddErrorListener(new ThrowingErrorListener<IToken>());
+
+				// https://stackoverflow.com/questions/63542345/how-to-detect-for-loop-block-after-parsing-the-code-using-antlr
+				TreeListner expressionWalker = new TreeListner();
+				ParseTreeWalker walker = new ParseTreeWalker();
+				// Walker
+				walker.Walk(expressionWalker, tree);
 
 			}
 			catch (Exception ex)
@@ -38,8 +123,12 @@ namespace prog_lang
 				Console.WriteLine(ex.ToString());
 			}
 
-			Console.Write("Press any key to continue...");
-			Console.ReadKey(true);
+			Console.WriteLine("Exiting...");
+			return 0;
+			//Console.Write("Press any key to continue...");
+			//Console.ReadKey(true);
 		}
+
 	}
+
 }
